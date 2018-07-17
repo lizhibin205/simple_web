eb5b08b4c11131be32f25053b9c0d0ccd7c28cd1
zhibin02.li <zhibin02.li@vipshop.com>
2018-07-17 16:23:41 +0800

    base spring webapp

---
+++ src/main/java/com/bytrees/HelloController.java
@@ -0,0 +1,16 @@
+package com.bytrees;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+public class HelloController {
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(ModelMap modelMap)
+    {
+		modelMap.addAttribute("message", "hello, spring web!");
+    	return "hello";
+    }
+}
