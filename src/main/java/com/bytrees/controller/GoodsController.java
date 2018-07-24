ca9fc763921f574eb7bd724716f94f9628b41b32
zhibin02.li <zhibin02.li@vipshop.com>
2018-07-24 22:10:41 +0800

    add jdbc

--- src/main/java/com/bytrees/controller/GoodsController.java
+++ src/main/java/com/bytrees/controller/GoodsController.java
@@ -2,7 +2,10 @@ package com.bytrees.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +16,22 @@ import com.bytrees.utils.ResponseJson;
 
 @RestController
 public class GoodsController {
-    @RequestMapping(value = "/goods/list", method = RequestMethod.GET, produces={"application/json;charset=UTF-8"})
-    public String getList() {
+    @RequestMapping(value = "/goods/simpleList", method = RequestMethod.GET, produces={"application/json;charset=UTF-8"})
+    public String getSimpleList() {
     	List<Goods> goodsList = new ArrayList<Goods>();
     	goodsList.add(new Goods(1, "测试商品"));
         goodsList.add(new Goods(2, "测试商品2"));
     	goodsList.add(new Goods(3, "蓝月亮洗衣液"));
         return JSON.toJSONString(new ResponseJson<List<Goods>>(200, "success", goodsList));
+    }
+
+    @RequestMapping(value = "/goods/list", method = RequestMethod.GET, produces={"application/json;charset=UTF-8"})
+    public String getList(JdbcTemplate jdbcTemplate) {
+    	try {
+    		List<Map<String, Object>> list =jdbcTemplate.queryForList("select * from goods");
+    		return JSON.toJSONString(new ResponseJson<List<Map<String, Object>>>(200, "", list));
+    	} catch (DataAccessException ex) {
+    		return JSON.toJSONString(new ResponseJson<Object>(500, ex.getMessage(), null));
+    	}
     }
 }
