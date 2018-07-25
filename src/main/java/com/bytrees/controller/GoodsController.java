e13d5e669b0d5e54ec76751ae4b6e803eeb0ed4e
zhibin02.li <zhibin02.li@vipshop.com>
2018-07-25 15:03:22 +0800

    update all config

--- src/main/java/com/bytrees/controller/GoodsController.java
+++ src/main/java/com/bytrees/controller/GoodsController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,9 @@ import com.bytrees.utils.ResponseJson;
 
 @RestController
 public class GoodsController {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
     @RequestMapping(value = "/goods/simpleList", method = RequestMethod.GET, produces={"application/json;charset=UTF-8"})
     public String getSimpleList() {
     	List<Goods> goodsList = new ArrayList<Goods>();
@@ -26,7 +30,7 @@ public class GoodsController {
     }
 
     @RequestMapping(value = "/goods/list", method = RequestMethod.GET, produces={"application/json;charset=UTF-8"})
-    public String getList(JdbcTemplate jdbcTemplate) {
+    public String getList() {
     	try {
     		List<Map<String, Object>> list =jdbcTemplate.queryForList("select * from goods");
     		return JSON.toJSONString(new ResponseJson<List<Map<String, Object>>>(200, "", list));
