93969c222b8e12c60fb434251465f47a3bdaa287
zhibin02.li <zhibin02.li@vipshop.com>
2018-08-02 11:38:14 +0800

    goods restful api

--- src/main/java/com/bytrees/controller/GoodsController.java
+++ src/main/java/com/bytrees/controller/GoodsController.java
@@ -1,6 +1,5 @@
 package com.bytrees.controller;
 
-import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +23,7 @@ public class GoodsController {
 		try {
 			Goods goods = goodsService.get(goodsId);
 			if (goods == null) {
-				throw new Exception("no goods found");
+				throw new Exception("Can't find goods(id=" + goodsId + ")");
 			}
 			return JSON.toJSONString(new ResponseJson<Goods>(200, "success", goods));
 		} catch (Exception ex) {
@@ -32,34 +31,15 @@ public class GoodsController {
     	}
 	}
 
-    @RequestMapping(value = "/goods/list", method = RequestMethod.GET, produces={"application/json;charset=UTF-8"})
-    public String getList(HttpServletRequest request) {
-    	try {
-    		int pageSize = 50;
-    		int page = 1;
-    		try {
-    			page = Integer.parseInt(request.getParameter("page"));
-    		} catch (NumberFormatException ex) {}
-    		if (page < 1) {
-    			page = 1;
-    		}
-    		int offset = (page - 1) * pageSize;
-    		List<Goods> goodsList = goodsService.getList(offset, pageSize);
-    		return JSON.toJSONString(new ResponseJson<List<Goods>>(200, "success", goodsList));
-    	} catch (Exception ex) {
-    		return JSON.toJSONString(new ResponseJson<Object>(500, ex.getMessage(), null));
-    	}
-    }
-
-    @RequestMapping(value = "/goods/create", method = RequestMethod.POST, produces={"application/json;charset=UTF-8"})
+    @RequestMapping(value = "/goods", method = RequestMethod.POST, produces={"application/json;charset=UTF-8"})
     public String create(HttpServletRequest request) {
     	try {
     		String goodsName = request.getParameter("name");
     		if (goodsName.length() == 0) {
-    			throw new Exception("goods name can't be null");
+    			throw new Exception("Goods name can't be null");
     		}
     		if (!goodsService.create(goodsName)) {
-    			throw new Exception("create goods failure");
+    			throw new Exception("Create goods failure");
     		}
     		return JSON.toJSONString(new ResponseJson<Object>(200, "success", null));
     	} catch (Exception ex) {
@@ -67,15 +47,15 @@ public class GoodsController {
     	}
     }
 
-    @RequestMapping(value = "/goods/update/{goodsId}", method = RequestMethod.PUT, produces={"application/json;charset=UTF-8"})
+    @RequestMapping(value = "/goods/{goodsId}", method = RequestMethod.PUT, produces={"application/json;charset=UTF-8"})
     public String update(@PathVariable(value="goodsId") int goodsId, HttpServletRequest request) {
     	try {
     		String goodsName = request.getParameter("name");
     		if (goodsName.length() == 0) {
-    			throw new Exception("goods name can't be null");
+    			throw new Exception("Goods name can't be null");
     		}
     		if (!goodsService.update(goodsId, goodsName)) {
-    			throw new Exception("update goods failure");
+    			throw new Exception("Update goods failure");
     		}
     		return JSON.toJSONString(new ResponseJson<Object>(200, "success", null));
     	} catch (Exception ex) {
@@ -83,11 +63,11 @@ public class GoodsController {
     	}
     }
 
-    @RequestMapping(value = "/goods/delete/{goodsId}", method = RequestMethod.DELETE, produces={"application/json;charset=UTF-8"})
+    @RequestMapping(value = "/goods/{goodsId}", method = RequestMethod.DELETE, produces={"application/json;charset=UTF-8"})
     public String delete(@PathVariable(value="goodsId") int goodsId) {
     	try {
     		if (!goodsService.delete(goodsId)) {
-    			throw new Exception("delete goods failure");
+    			throw new Exception("Delete goods failure");
     		}
     		return JSON.toJSONString(new ResponseJson<Object>(200, "success", null));
     	} catch (Exception ex) {
