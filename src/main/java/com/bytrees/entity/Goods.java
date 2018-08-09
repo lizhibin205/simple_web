97db38bfca3832ab034ac872552367b8655ec6d7
zhibin02.li <zhibin02.li@vipshop.com>
2018-08-09 14:52:49 +0800

    goods id Long

--- src/main/java/com/bytrees/entity/Goods.java
+++ src/main/java/com/bytrees/entity/Goods.java
@@ -1,13 +1,13 @@
 package com.bytrees.entity;
 
 public class Goods {
-    private Integer id;
+    private Long id;
     private String name;
 
-    public Integer getId() {
+    public Long getId() {
     	return id;
     }
-    public void setId(Integer id) {
+    public void setId(Long id) {
     	this.id = id;
     }
 
