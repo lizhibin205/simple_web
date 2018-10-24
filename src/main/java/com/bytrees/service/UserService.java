826419a332ead51896043f62c2b3d0573d629afe
zhibin02.li <zhibin02.li@vipshop.com>
2018-10-24 14:39:11 +0800

    config

--- src/main/java/com/bytrees/service/UserService.java
+++ src/main/java/com/bytrees/service/UserService.java
@@ -1,10 +1,12 @@
 package com.bytrees.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.bytrees.entity.User;
 import com.bytrees.mapper.UserMapper;
 
+@Component
 public class UserService {
 	@Autowired
     private UserMapper userMapper;
