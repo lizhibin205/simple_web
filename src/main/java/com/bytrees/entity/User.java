a90f8b1d46dc8ae3ff763b11b72d0c861438d571
zhibin02.li <zhibin02.li@vipshop.com>
2018-10-24 15:03:10 +0800

    config

--- src/main/java/com/bytrees/entity/User.java
+++ src/main/java/com/bytrees/entity/User.java
@@ -2,26 +2,10 @@ package com.bytrees.entity;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name="users")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue
-	@Column(name="id")
     private Long id;
-
-	@Column(name="username")
 	private String username;
-
-	@Column(name="password")
 	private String password;
 
 	public Long getId() {
