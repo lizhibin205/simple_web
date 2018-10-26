2994848458fa4278fa0804312eaea598d412b180
zhibin02.li <zhibin02.li@vipshop.com>
2018-10-26 20:04:32 +0800

    redirect demo

--- src/main/java/com/bytrees/controller/HelloController.java
+++ src/main/java/com/bytrees/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.bytrees.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,5 +15,15 @@ public class HelloController {
     	String message = "hello, spring web!";
         modelMap.addAttribute("message", message);
         return "hello";
+    }
+
+    @RequestMapping(value = "/hello/redirect", method = RequestMethod.GET)
+    public String redirect(HttpServletResponse response) {
+    	try {
+    		response.sendRedirect("/hello");
+    	} catch (Exception ex) {
+    		
+    	}
+    	return null;
     }
 }
